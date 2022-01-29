c7cf10f71f2f4c5f52e240ddf2c0be0b5cc2e2bf
N3NU <stevencaraballo27@gmail.com>
2022-01-28 21:03:35 -0500

    Changes made to subdom_enum.py and subdomains.txt

--- subdom_enum.py
+++ subdom_enum.py
@@ -6,6 +6,22 @@ import sys
 
 #Assigning varible with file content
 subdomain_list = open("subdomains.txt").read()
-#Assigning varible with 
+#Assigning varible with a list of lines from subdomain_list
 subdomains = subdomain_list.splitlines()
-print(subdomain_list)
+
+#Iterating through the list of words in subdomains variable
+for sub in subdomains:
+    #Assigning the variable with FQDN
+    new_subdomains = f"http://{sub}.{sys.argv[1]}"
+    print(new_subdomains)
+    #Check to see if new_subdomains give valid response
+    try:
+        requests.get(new_subdomains)
+
+    #Except ConnectionError if it occurs
+    except requests.ConnectionError:
+        pass
+    
+    #If new_subdomains has valid response, print
+    else:
+        print("Valid domain: ",new_subdomains)
